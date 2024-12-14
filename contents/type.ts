053ef3a6673a3223e96502d047ac4e01deb185b3
ToYama <toyamasoujinn@gmail.com>
2024-12-14 22:37:47 +0900

    型定義をもとに戻した

--- contents/type.ts
+++ contents/type.ts
@@ -1,14 +1,16 @@
-export type datePeriod = {
-  date: "月" | "火" | "水" | "木" | "金" | "土" | "日"
-  period: number
-}
+type date = "月" | "火" | "水" | "木" | "金" | "土" | "日"
+type period = number
 export type yearQuoter = {
   year: number
   quoter: 1 | 2 | 3 | 4
 }
+export type datePeriod = {
+  date: date
+  period: period
+}
 export type course = {
-  yearQuoter: yearQuoter
   datePeriod: datePeriod
+  yearQuoter: yearQuoter
   courseNumber: string
   courseName: string
   onClick: any
