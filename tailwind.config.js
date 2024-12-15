2e2b29fa868afcf411dd42d60165aef531877bcd
ToYama <toyamasoujinn@gmail.com>
2024-12-16 00:10:12 +0900

    修正 extendにcolorsを入れるとtailwind既定の色も使えるようになるため

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,20 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
-    colors: {
-      acanthus: {
-        bright: "#088ab4",
-        DEFAULT: "#003e5f"
+    extend: {
+      colors: {
+        acanthus: {
+          bright: "#088ab4",
+          DEFAULT: "#003e5f"
+        },
+        white: {
+          DEFAULT: "#ffffff",
+          dark: "#fafafa"
+        }
       }
     }
   },
   mode: "jit",
   darkMode: "class",
-  plugins: []
+  plugins: [require("tailwindcss-radix")]
 }
